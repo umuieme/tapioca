74d6abc2663b454eb73d0045cf66ee3089ecc255
SAILES <sailes@SAILESs-MacBook-Pro.local>
2021-07-29 14:22:29 +0545

    size

--- android/src/main/kotlin/me/anharu/video_editor/filter/GlImageOverlayFilter.kt
+++ android/src/main/kotlin/me/anharu/video_editor/filter/GlImageOverlayFilter.kt
@@ -1,5 +1,6 @@
 package me.anharu.video_editor.filter
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import com.daasuu.mp4compose.filter.GlOverlayFilter
@@ -9,6 +10,8 @@ class GlImageOverlayFilter(imageOverlay: ImageOverlay) : GlOverlayFilter() {
     private val imageOverlay: ImageOverlay = imageOverlay;
 
     protected override fun drawCanvas(canvas: Canvas) {
-        canvas.drawBitmap(BitmapFactory.decodeByteArray(imageOverlay.bitmap, 0, imageOverlay.bitmap.size), imageOverlay.x.toFloat(), imageOverlay.y.toFloat(), null);
-    }
+        var b = BitmapFactory.decodeByteArray (imageOverlay.bitmap, 0, imageOverlay.bitmap.size)
+        var bitmap= Bitmap.createScaledBitmap(b, b.width/3, b.height/3, false);
+        canvas.drawBitmap(bitmap, imageOverlay.x.toFloat(), imageOverlay.y.toFloat(), null);
+          }
 }
