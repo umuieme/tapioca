3b3df04b6bba3de29df7f3bf1d8a313d0d33d8ea
SAILES <sailes@SAILESs-MacBook-Pro.local>
2021-07-30 12:36:03 +0545

    change  to cancas

--- android/src/main/kotlin/me/anharu/video_editor/filter/GlImageOverlayFilter.kt
+++ android/src/main/kotlin/me/anharu/video_editor/filter/GlImageOverlayFilter.kt
@@ -1,17 +1,31 @@
 package me.anharu.video_editor.filter
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
+import android.R.attr.path
+import android.graphics.*
 import com.daasuu.mp4compose.filter.GlOverlayFilter
 import me.anharu.video_editor.ImageOverlay
+
 
 class GlImageOverlayFilter(imageOverlay: ImageOverlay) : GlOverlayFilter() {
     private val imageOverlay: ImageOverlay = imageOverlay;
 
     protected override fun drawCanvas(canvas: Canvas) {
         var b = BitmapFactory.decodeByteArray (imageOverlay.bitmap, 0, imageOverlay.bitmap.size)
-        var bitmap= Bitmap.createScaledBitmap(b, b.width/10, b.height/10, false);
-        canvas.drawBitmap(bitmap, imageOverlay.x.toFloat(), imageOverlay.y.toFloat(), null);
-          }
+         var bitmap= scaleBitmap(b, b.width, b.height);
+//
+        // var bitmap=Bitmap.createScaledBitmap(b, b.width, b.height, true);
+
+        canvas.drawBitmap(bitmap, imageOverlay.x.toFloat(), imageOverlay.y.toFloat(), Paint(2));
+//        canvas.scale(2f, 2f);
+
+
+    }
+    fun scaleBitmap(bitmap: Bitmap, wantedWidth: Int, wantedHeight: Int): Bitmap {
+        val output = Bitmap.createBitmap(wantedWidth, wantedHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(output)
+        val m = Matrix()
+        m.setScale(wantedWidth.toFloat() / bitmap.width, wantedHeight.toFloat() / bitmap.height)
+        canvas.drawBitmap(bitmap, m, Paint())
+        return output
+    }
 }
